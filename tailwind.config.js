88b24a09d557c560b607bfd87b84b86c138de22a
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-07-23 10:52:34 +0530

    feat: Define global colors and font in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          light: '#6366F1',
+          DEFAULT: '#4F46E5',
+          dark: '#4338CA',
+        },
+        secondary: {
+          light: '#FDE047',
+          DEFAULT: '#FACC15',
+          dark: '#EAB308',
+        },
+      },
+    },
   },
   plugins: [],
 }
